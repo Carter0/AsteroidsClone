2f8922bfab8aa418cc078b92a6d3368d9fa3271c
carter <weinbergcarter@gmail.com>
2021-08-13 09:13:23 -0400

    The player now moves, good job!

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(add_camera.system())
         .add_startup_system(spawn_player.system())
+        .add_system(move_player.system())
         .run();
 }
 
@@ -23,11 +24,13 @@ fn add_camera(mut commands: Commands) {
 }
 
 // PLAYER CODE
-struct Player;
 
-fn spawn_player(mut commands: Commands,
-                mut materials: ResMut<Assets<ColorMaterial>>) {
+// The float value is the player movement speed in 'pixels/second'.
+struct Player {
+    velocity: f32,
+}
 
+fn spawn_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     let sprite_size_x = 40.0;
     let sprite_size_y = 40.0;
 
@@ -38,5 +41,28 @@ fn spawn_player(mut commands: Commands,
             sprite: Sprite::new(Vec2::new(sprite_size_x, sprite_size_y)),
             ..Default::default()
         })
-        .insert(Player);
+        .insert(Player { velocity: 10.0 });
+}
+
+fn move_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<(&Player, &mut Transform)>,
+) {
+    let (player, mut transform) = player_query
+        .single_mut()
+        .expect("There should always be exactly one player in the game!");
+
+    // Get input from the keyboard (WASD)
+    let up: bool = keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up);
+    let down: bool = keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down);
+    let left: bool = keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left);
+    let right: bool = keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right);
+
+    // If left is pressed than it will be -1, right 1, both they cancel out.
+    let x_axis: i8 = -(left as i8) + right as i8;
+    let y_axis: i8 = -(down as i8) + up as i8;
+    let move_delta: Vec2 = Vec2::new(x_axis as f32, y_axis as f32);
+
+    transform.translation.x += move_delta.x * player.velocity;
+    transform.translation.y += move_delta.y * player.velocity;
 }
