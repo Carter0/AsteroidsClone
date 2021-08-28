ea80653bafb742e186ffe6493684bc9751c0665a
carter <weinbergcarter@gmail.com>
2021-08-28 10:31:15 -0400

    Adding block code! Still more to do there

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::*;
+use rand::Rng;
+use rand::distributions::{Distribution, Standard};
 
 const WINDOWHEIGHT: f32 = 900.0;
-const WINDOWWIDTH: f32 = 700.0;
+const WINDOWWIDTH: f32 = 1000.0;
 
 fn main() {
     App::build()
@@ -17,6 +19,7 @@ fn main() {
         .add_startup_system(spawn_player.system())
         .add_startup_system(spawn_block.system())
         .add_system(move_player.system())
+        .add_system(move_blocks.system())
         .run();
 }
 
@@ -110,14 +113,39 @@ fn move_player(
 }
 
 // Block Code
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down
+}
+
+impl Distribution<Direction> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Direction {
+        match rng.gen_range(0..=3) {
+            0 => Direction::Left,
+            1 => Direction::Right,
+            2 => Direction::Up,
+            _ => Direction::Down
+        }
+    }
+}
 
 struct Block {
     velocity: f32,
+    direction: Direction
 }
 
+// TODO at some point the number of blocks is going to need to be randomly generated
+// TODO also the position of the blocks needs to be randomly generated
+// TODO also the size of the blocks needs to be randomly generated
+// TODO also clean up the code using a loop, maybe a struct with a list of blocks
 fn spawn_block(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     let sprite_size_x = 80.0;
     let sprite_size_y = 80.0;
+
+    let rand_direction: Direction = rand::random();
+    let rand_direction2: Direction = rand::random();
 
     commands
         .spawn_bundle(SpriteBundle {
@@ -126,5 +154,45 @@ fn spawn_block(mut commands: Commands, mut materials: ResMut<Assets<ColorMateria
             sprite: Sprite::new(Vec2::new(sprite_size_x, sprite_size_y)),
             ..Default::default()
         })
-        .insert(Block { velocity: 10.0 });
+        .insert(Block { velocity: 10.0, direction: rand_direction });
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.add(Color::rgb(1.0, 0.5, 1.0).into()),
+            transform: Transform::from_xyz(0.0, 300.0, 1.0),
+            sprite: Sprite::new(Vec2::new(sprite_size_x, sprite_size_y)),
+            ..Default::default()
+        })
+        .insert(Block { velocity: 10.0, direction: rand_direction2 });
+}
+
+fn move_blocks(mut block_query: Query<(&Block, &mut Transform, &Sprite)>) {
+
+    // move the block by its own velocity
+    for (block, mut transform, sprite) in block_query.iter_mut() {
+        match &block.direction {
+            Direction::Left => transform.translation.x -= block.velocity,
+            Direction::Right => transform.translation.x += block.velocity,
+            Direction::Up => transform.translation.y += block.velocity,
+            Direction::Down => transform.translation.y -= block.velocity
+        };
+
+        // Wrap the block if they go off screen
+        if transform.translation.x > WINDOWWIDTH / 2.0 + sprite.size.x {
+            transform.translation.x = -WINDOWWIDTH / 2.0;
+        }
+
+        if transform.translation.x < -WINDOWWIDTH / 2.0 - sprite.size.x {
+            transform.translation.x = WINDOWWIDTH / 2.0;
+        }
+
+        if transform.translation.y > WINDOWHEIGHT / 2.0 + sprite.size.y {
+            transform.translation.y = -WINDOWHEIGHT / 2.0;
+        }
+
+        if transform.translation.y < -WINDOWHEIGHT / 2.0 - sprite.size.y {
+            transform.translation.y = WINDOWHEIGHT / 2.0;
+        }
+    }
+
 }
