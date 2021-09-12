8618ac309d37efd3ab4e044fa16310edafdf110f
carter <weinbergcarter@gmail.com>
2021-09-12 15:06:44 -0400

    Still doing research

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,8 @@ fn add_camera(mut commands: Commands) {
 }
 
 // Overall Info
+// TODO refactor to use a vector of blocks
+// then spawn the number in the vector
 struct Game {
     blocks : Vec<Block>
 }
@@ -184,25 +186,46 @@ struct Block {
     direction: Direction,
 }
 
-fn spawn_starting_block(commands: Commands, materials: ResMut<Assets<ColorMaterial>>) {
-
+// NOTE
+// I tried refactoring the below block spawning functions to call a helper method since
+// much of the logic is the same. I ran into many ownership issues by doing that and it made me being to
+// reconsider the architecture of the app.
+fn spawn_starting_block(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     let mut counter = 0;
 
     let block_number = 20;
     while counter < block_number {
-        spawn_block(commands, materials, Color::rgb(1.0, 0.5, 1.0));
+        let color = Color::rgb(1.0, 0.5, 1.0);
+        let mut rng = thread_rng();
+        let rand_direction: Direction = rand::random();
+        let x_starting_position = rng.gen_range(0.0..=WINDOWWIDTH);
+        let y_starting_position = rng.gen_range(0.0..=WINDOWHEIGHT);
+        let sprite_size_x = 80.0;
+        let sprite_size_y = 80.0;
+
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.add(color.into()),
+                transform: Transform::from_xyz(x_starting_position, y_starting_position, 1.0),
+                sprite: Sprite::new(Vec2::new(sprite_size_x, sprite_size_y)),
+                ..Default::default()
+            })
+            .insert(Block {
+                velocity: 300.0,
+                direction: rand_direction,
+            })
+            .insert(Collidable);
+
         counter += 1;
-    }
+        }
 }
 
 // spawns blocks as a way to make the game harder during runtime
 // this will only run every spawn block timestep
-fn spawn_runtime_blocks(commands: Commands,
-                        materials: ResMut<Assets<ColorMaterial>>) {
-    spawn_block(commands, materials, Color::rgb(0.2, 0.5, 1.0))
-}
+fn spawn_runtime_blocks(mut commands: Commands,
+                        mut materials: ResMut<Assets<ColorMaterial>>) {
 
-fn spawn_block(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>, color : Color) {
+    let color = Color::rgb(0.2, 0.5, 1.0);
     let mut rng = thread_rng();
     let rand_direction: Direction = rand::random();
     let x_starting_position = rng.gen_range(0.0..=WINDOWWIDTH);
@@ -223,6 +246,29 @@ fn spawn_block(mut commands: Commands, mut materials: ResMut<Assets<ColorMateria
         })
         .insert(Collidable);
 }
+
+// NOTE commented out until I can do some more research on this
+// fn spawn_block(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>, color : Color) {
+//     let mut rng = thread_rng();
+//     let rand_direction: Direction = rand::random();
+//     let x_starting_position = rng.gen_range(0.0..=WINDOWWIDTH);
+//     let y_starting_position = rng.gen_range(0.0..=WINDOWHEIGHT);
+//     let sprite_size_x = 80.0;
+//     let sprite_size_y = 80.0;
+
+//     commands
+//         .spawn_bundle(SpriteBundle {
+//             material: materials.add(color.into()),
+//             transform: Transform::from_xyz(x_starting_position, y_starting_position, 1.0),
+//             sprite: Sprite::new(Vec2::new(sprite_size_x, sprite_size_y)),
+//             ..Default::default()
+//         })
+//         .insert(Block {
+//             velocity: 300.0,
+//             direction: rand_direction,
+//         })
+//         .insert(Collidable);
+// }
 
 // move the block by its own velocity
 fn move_blocks(mut block_query: Query<(&Block, &mut Transform, &Sprite)>, time: Res<Time>) {
