2913fd7a82a0275625923e20764f6eb519fdf9c4
carter <weinbergcarter@gmail.com>
2021-10-30 19:23:40 -0400

    Refactoring code and working on bugs

--- src/logic/blocks.rs
+++ src/logic/blocks.rs
@@ -1,13 +1,15 @@
 // BLOCKS CODE
 
-use crate::logic::spawning::{SpawnInfo, SpawnList};
+// use crate::logic::spawning::{SpawnInfo};
 use crate::{Collidable, Direction, WINDOWHEIGHT, WINDOWWIDTH};
 
-use bevy::core::FixedTimestep;
+// use bevy::core::FixedTimestep;
 use bevy::prelude::*;
 use rand::distributions::{Distribution, Standard};
-use rand::seq::IteratorRandom;
+// use rand::seq::IteratorRandom;
 use rand::{thread_rng, Rng};
+
+use super::spawning::SpawnInfo;
 
 // For BLOCK_SPAWN_TIMESTEP, it's once every two seconds
 const BLOCK_SPAWN_TIMESTEP: f64 = 120.0 / 60.0;
@@ -19,11 +21,11 @@ impl Plugin for BlocksPlugin {
         app
             // Needs to be run after spawning logic
             .add_startup_system_to_stage(StartupStage::PostStartup, spawn_starting_block.system())
-            .add_system_set(
-                SystemSet::new()
-                    .with_run_criteria(FixedTimestep::step(BLOCK_SPAWN_TIMESTEP))
-                    .with_system(spawn_runtime_blocks.system()),
-            )
+            // .add_system_set(
+            //     SystemSet::new()
+            //         .with_run_criteria(FixedTimestep::step(BLOCK_SPAWN_TIMESTEP))
+            //         .with_system(spawn_runtime_blocks.system()),
+            // )
             .add_system(move_blocks.system());
     }
 }
@@ -47,78 +49,107 @@ struct Block {
 // Spawns starting blocks for the game
 fn spawn_starting_block(
     mut commands: Commands,
-    mut spawn_positions_query: Query<&mut SpawnList>,
+    mut spawn_positions_query: Query<&mut SpawnInfo>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let mut spawn_positions = spawn_positions_query
-        .single_mut()
-        .expect("There should only be one instance of spawn positions");
-
-    let mut counter = 0;
-
-    let block_number = 5;
-    while counter < block_number {
+    // TODO
+    // Nothing spawns because you only have a vec of spawninfo in the world
+    // you don't have individual spawn infos
+    let block_number = 40;
+    for mut init_spawn_positions in spawn_positions_query.iter_mut().take(block_number) {
         spawn_block(
             &mut commands,
             &mut materials,
-            &mut spawn_positions,
+            &mut init_spawn_positions,
             Color::rgb(1.0, 0.5, 1.0),
         );
-        counter += 1;
     }
+
+    // let mut counter = 0;
+
+    // let block_number = 5;
+    // while counter < block_number {
+    //     spawn_block(
+    //         &mut commands,
+    //         &mut materials,
+    //         &mut spawn_positions,
+    //         Color::rgb(1.0, 0.5, 1.0),
+    //     );
+    //     counter += 1;
+    // }
 }
 
 // spawns blocks as a way to make the game harder during runtime
 // this will only run every spawn block timestep
-fn spawn_runtime_blocks(
-    mut commands: Commands,
-    mut spawn_positions_query: Query<&mut SpawnList>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    let mut spawn_positions = spawn_positions_query
-        .single_mut()
-        .expect("There should only be one instance of spawn positions");
+// fn spawn_runtime_blocks(
+//     mut commands: Commands,
+//     mut spawn_positions_query: Query<&mut SpawnList>,
+//     mut materials: ResMut<Assets<ColorMaterial>>,
+// ) {
+//     let mut spawn_positions = spawn_positions_query
+//         .single_mut()
+//         .expect("There should only be one instance of spawn positions");
 
-    spawn_block(
-        &mut commands,
-        &mut materials,
-        &mut spawn_positions,
-        Color::rgb(0.2, 0.5, 1.0),
-    );
-}
+//     spawn_block(
+//         &mut commands,
+//         &mut materials,
+//         &mut spawn_positions,
+//         Color::rgb(0.2, 0.5, 1.0),
+//     );
+// }
 
-fn get_list_orientation(integer: i8, spawn_list: &SpawnList) -> &Vec<SpawnInfo> {
-    match integer {
-        1 => &spawn_list.horizontal_list,
-        _ => &spawn_list.vertical_list,
-    }
-}
+// fn get_list_orientation(integer: i8, spawn_list: &mut SpawnList) -> &mut Vec<SpawnInfo> {
+//     match integer {
+//         1 => &mut spawn_list.horizontal_list,
+//         _ => &mut spawn_list.vertical_list,
+//     }
+// }
 
+// TODO
+// this should only be called if there are still blocks left to spawn
 fn spawn_block(
     commands: &mut Commands,
     materials: &mut ResMut<Assets<ColorMaterial>>,
-    spawn_positions: &mut SpawnList,
+    spawn_position: &mut SpawnInfo,
     color: Color,
 ) {
-    let mut rng = thread_rng();
+    // let mut rng = thread_rng();
 
     // Randomly pick a position based on whether its been spawned or not
-    let random_position: &SpawnInfo = get_list_orientation(rng.gen_range(0..=1), &spawn_positions)
-        .iter()
-        .filter(|spawn_position| spawn_position.spawned == false)
-        .choose(&mut rng)
-        .unwrap();
+    // let random_position: &mut SpawnInfo = spawn_positions
+    //     .iter_mut()
+    //     .filter(|spawn_position| spawn_position.spawned == false)
+    //     .choose(&mut rng)
+    //     .unwrap();
 
-    let sprite_size_x = 80.0;
-    let sprite_size_y = 80.0;
-    let location = random_position.spawn_location;
-    let direction = random_position.spawn_direction;
+    // TODO
+    // These needs to be consts somewhere outside
+    // this function because the spawning code uses it
+    let sprite_size_x = 40.0;
+    let sprite_size_y = 40.0;
+
+    let location = spawn_position.spawn_location;
+    let direction = spawn_position.spawn_direction;
+
+    // set the positions spawned value to true
+    spawn_position.spawned = true;
 
     // TODO
     // Also you never set spawned to true lol
+    //
+    // TODO
+    // I think the block limit then respawn a good idea
 
     // NOTE
     // honestly I kinda like it without touching the spawn bool
+
+    // TODO
+    // Bug with staying in the corner
+    //
+    //
+    // TODO stop score accumulation
+    //
+    // TODO Respawn button
 
     println!("x position: {}", location.0);
     println!("y position: {}", location.1);
@@ -127,7 +158,7 @@ fn spawn_block(
         .spawn_bundle(SpriteBundle {
             sprite: Sprite::new(Vec2::new(sprite_size_x, sprite_size_y)),
             material: materials.add(color.into()),
-            transform: Transform::from_xyz(location.1 as f32, location.0 as f32, 1.0),
+            transform: Transform::from_xyz(location.0 as f32, location.1 as f32, 1.0),
             ..Default::default()
         })
         .insert(Block {
--- src/logic/spawning.rs
+++ src/logic/spawning.rs
@@ -32,6 +32,13 @@ impl fmt::Display for Direction {
     }
 }
 
+// TODO
+// I feel like the below can be simplified greatly
+// by removing spawnlists
+//
+// I think that the only benefit they provide is indicating whether something is horizontal or
+// vertical, which can be contained in the SpawnInfo Struct.
+
 // Contains the locations and directions that
 // a block can be spawned in as well as whether
 // that position is spawned or not
@@ -40,14 +47,15 @@ pub struct SpawnInfo {
     pub spawn_location: (i16, i16),
     pub spawn_direction: Direction,
     pub spawned: bool,
+    pub direction: BlockDirection,
 }
 
 // Contains the spawn locations for all the blocks
 // Will be used to actually spawn the blocks later
-pub struct SpawnList {
-    pub horizontal_list: Vec<SpawnInfo>,
-    pub vertical_list: Vec<SpawnInfo>,
-}
+// pub struct SpawnList {
+//     pub horizontal_list: Vec<SpawnInfo>,
+//     pub vertical_list: Vec<SpawnInfo>,
+// }
 
 impl fmt::Display for SpawnInfo {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -59,11 +67,13 @@ impl fmt::Display for SpawnInfo {
     }
 }
 
-enum BlockDirection {
+#[derive(Clone, Copy)]
+pub enum BlockDirection {
     Horizontal,
     Vertical,
 }
 
+// spawning at 500
 fn get_edge_of_screen(window_size: f32) -> i16 {
     window_size as i16 / 2
 }
@@ -81,30 +91,38 @@ fn create_random_blocks<R: Rng>(
                 BlockDirection::Vertical => get_direction(rng.gen_range(3..=4)),
             },
             spawn_location: match orientation {
-                BlockDirection::Horizontal => (*block_position, get_edge_of_screen(WINDOWWIDTH)),
-                BlockDirection::Vertical => (get_edge_of_screen(WINDOWHEIGHT), *block_position),
+                // If you want the blocks to go horizontally (left/right) the y position needs to change
+                BlockDirection::Horizontal => (get_edge_of_screen(WINDOWWIDTH), *block_position),
+                // If you want the blocks to go vertically (up/down) the x position needs to change
+                BlockDirection::Vertical => (*block_position, get_edge_of_screen(WINDOWHEIGHT)),
             },
             spawned: false,
+            direction: orientation
         })
         .collect();
 }
 
 // Blocks should spawn in set locations depending on
 // the size of the screen.
-fn create_spawn_locations() -> SpawnList {
+fn create_spawn_locations() -> Vec<SpawnInfo> {
     // Calculate the number of blocks per side
     // ScreenLength / (BlockGap + BlockLength) = BlockNumber
-    let blocks_per_width: i16 = WINDOWWIDTH as i16 / (45 + 80);
-    let blocks_per_height: i16 = WINDOWHEIGHT as i16 / (45 + 80);
+    // Plus 1 because I want one block to spawn at the opposite edge
+    let blocks_per_width: i16 = WINDOWWIDTH as i16 / (45 + 80) + 1;
+    let blocks_per_height: i16 = WINDOWHEIGHT as i16 / (45 + 80) + 1;
 
     // Calculate the positions of the blocks per side
     // Need to divide by half because (0,0) is the middle of the screen
-    let block_width_positions: Vec<i16> = (1..=blocks_per_width)
+    // TODO
+    // Redo the names to horizontal and vertical positions because
+    // the width and height thing is confusing you.
+    // There are horizontal and vertical blocks, not positions here
+    let block_width_positions: Vec<i16> = (0..=blocks_per_width)
         .map(|x| WINDOWWIDTH as i16 - (x * 90))
         .map(|x| x - get_edge_of_screen(WINDOWWIDTH))
         .collect();
 
-    let block_height_positions: Vec<i16> = (1..=blocks_per_height)
+    let block_height_positions: Vec<i16> = (0..=blocks_per_height)
         .map(|y| WINDOWHEIGHT as i16 - (y * 90))
         .map(|y| y - get_edge_of_screen(WINDOWHEIGHT))
         .collect();
@@ -119,10 +137,8 @@ fn create_spawn_locations() -> SpawnList {
         create_random_blocks(block_height_positions, BlockDirection::Vertical, &mut rng);
 
     // Combine the blocks together
-    SpawnList {
-        horizontal_list: random_horizontal_blocks,
-        vertical_list: random_vertical_blocks,
-    }
+    [random_horizontal_blocks, random_vertical_blocks].concat()
+
 }
 
 // Create a list of spawn block locations and
@@ -132,6 +148,11 @@ fn create_spawn_locations() -> SpawnList {
 // Command usage is only applied between stages,
 // so anything that wants to access this data needs to do so in a later
 // stage.
+//
+// TODO
+// In Bevy 0.6 this can be rewritten using IteratorCommands
 fn spawn_block_positions(mut commands: Commands) {
-    commands.spawn().insert(create_spawn_locations());
+    for spawn_location in create_spawn_locations() {
+        commands.spawn().insert(spawn_location);
+    }
 }
